fc59cfd787a9b32fa3bfbdbaa8d020c2f4d89239
Kazumakaji <zm18007@josai.ac.jp>
2020-08-07 03:52:51 +0000

    0807

--- image/myjs.js
+++ image/myjs.js
@@ -1,10 +1,14 @@
-//alert('Hello,Osamu!');
+//alrt('Hello,Osamu!');
 
-alert(1+1);
+//alert(1+1);
 
+
+var x = Math.sqrt(2);
+
+var x = Math.sqrt(3);
 alert(Math.sqrt(1+1));
 
  Math.sqrt(2+3);
  
-
+alart;
 
