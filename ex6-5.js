faa0d76c2db18656fc1faf3ae5ae7d14810cc744
Kazumakaji <zm18007@josai.ac.jp>
2020-10-23 09:41:06 +0000

    1023

--- ex6-5.js
+++ ex6-5.js
@@ -1,13 +1,15 @@
 function sayhello2(){
-    var who = 'world';
+    var  who= '太郎';'花子';
     alert('Hello, ' + who + '!');
 }
 
 function taro(){
     who = '太郎';
+    alert('が来た');
 }
 
 function hanako(){
     who = '花子';
+    alert('が来た');
 }
 
