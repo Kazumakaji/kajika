91d86c5fdccd7e7b8d4ab3b298f762e641e85daa
Kazumakaji <zm18007@josai.ac.jp>
2020-10-27 02:09:44 +0000

    1027

--- ex6-5.js
+++ ex6-5.js
@@ -1,6 +1,7 @@
+var  who65　='world';
+var  who650 ='が来た';
 function sayhello65(){
-    var  who65　='world';
-    alert('Hello, ' + who65 + '!');
+    alert('Hello, ' + who65 + who650 +'!');
 }
 
 function taro65(){
@@ -9,5 +10,9 @@ function taro65(){
 
 function hanako65(){
     who65 = '花子';
+}
+function  gakita(){
+    //who650 = 'が来た';
+    alert(who65 + 'が来た');
 }
 
