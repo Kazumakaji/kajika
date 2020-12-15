16807e9be5d935ce66b8da77d995823e07b3d483
shunsukekanno <zm19094@josai.ac.jp>
2020-12-15 02:43:59 +0000

    1215

--- ex06-11.js
+++ ex06-11.js
@@ -1,3 +1,10 @@
-function ChangeImageAndBack() {
-    document.getElementById("img13").src= './image/kuma.png';
+//function ChangeImageAndBack() {
+    //document.getElementById('img13').src= './image/kuma.png';
+//}
+//setTimeout("ChangeImageAndBack", 3000);
+
+function imgchange2() {
+    document.getElementById('change').src= './image/josai.png';
 }
+
+    //onclick=setTimeout("imgchange2()", 3000);
